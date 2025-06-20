c78261b95ac2638a844448f7c75a17abf11e9603
anhquannewbie <anhquan08114@gmail.com>
2025-06-20 14:05:10 +0700

    Switch DATABASES to use DATABASE_URL with dj-database-url

--- meals_system/settings.py
+++ meals_system/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,10 +56,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'meals_system.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'site.db',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'site.db'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
